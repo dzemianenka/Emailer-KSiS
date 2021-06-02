00b10e895c4642d3456c18cc8152a2dfd751248f
demosocket <dzemianenka.aliaksei@gmail.com>
2021-06-02 15:39:26 +0300

    Refactoring MessageMapper

--- src/main/java/com/demosocket/emailer/mapper/MessageMapper.java
+++ src/main/java/com/demosocket/emailer/mapper/MessageMapper.java
@@ -13,37 +13,37 @@ import org.springframework.stereotype.Component;
 @Component
 public class MessageMapper {
 
-  public InboxMail toInboxMail(IMAPMessage message) {
-    try {
-      InboxMail inboxMail = new InboxMail();
-      inboxMail.setFrom(getFrom(message));
-      inboxMail.setDate(getDate(message));
-      inboxMail.setSubject(getSubject(message));
+    public InboxMail toInboxMail(IMAPMessage message) {
+        try {
+            InboxMail inboxMail = new InboxMail();
+            inboxMail.setFrom(getFrom(message));
+            inboxMail.setDate(getDate(message));
+            inboxMail.setSubject(getSubject(message));
 
-      return inboxMail;
-    } catch (Exception e) {
-      System.out.println(e.getMessage());
-      return null;
+            return inboxMail;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
-  }
 
-  private String getFrom(IMAPMessage message) throws MessagingException {
-    return Arrays.stream(message.getFrom())
-        .map(a -> (InternetAddress) a)
-        .map(InternetAddress::getAddress)
-        .collect(Collectors.joining(", "));
-  }
-
-  private LocalDateTime getDate(IMAPMessage message) throws MessagingException {
-    return message.getSentDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
-  }
-
-  private String getSubject(IMAPMessage message) {
-    try {
-      return message.getSubject();
-    } catch (MessagingException e) {
-      return "N/A";
+    private String getFrom(IMAPMessage message) throws MessagingException {
+        return Arrays.stream(message.getFrom())
+            .map(a -> (InternetAddress) a)
+            .map(InternetAddress::getAddress)
+            .collect(Collectors.joining(", "));
     }
-  }
+
+    private LocalDateTime getDate(IMAPMessage message) throws MessagingException {
+        return message.getSentDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+    }
+
+    private String getSubject(IMAPMessage message) {
+        try {
+            return message.getSubject();
+        } catch (MessagingException e) {
+            return "N/A";
+        }
+    }
 
 }
