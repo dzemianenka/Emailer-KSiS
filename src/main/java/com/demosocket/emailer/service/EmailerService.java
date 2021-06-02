99b9574cac0beaccdb49597c6f58285747196fc4
demosocket <demosocket@gmail.com>
2021-06-02 18:46:39 +0300

    Deleted annotation service

--- src/main/java/com/demosocket/emailer/service/EmailerService.java
+++ src/main/java/com/demosocket/emailer/service/EmailerService.java
@@ -3,11 +3,9 @@ package com.demosocket.emailer.service;
 import com.demosocket.emailer.model.InboxAuth;
 import com.demosocket.emailer.model.InboxMail;
 import com.demosocket.emailer.model.Mail;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
 public interface EmailerService {
 
     void send(Mail mail) throws Exception;
